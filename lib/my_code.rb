5e7c996bef2c9991abdf2680e7af85b131a1723d
Franky Liu <cuifranky95@gmail.com>
2019-08-21 18:18:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,8 +7,8 @@ def map(array)
   return newarray
 end
 
-def reduce(array, starting_point = 0)
-  newvalue = !!starting_point
+def reduce(array, starting_point)
+  newvalue = starting_point
   array.each do |index|
     newvalue = yield(newvalue, index)
   end
